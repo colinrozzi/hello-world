ab39b332d7d6c5ddab736e5909a190134ea17360
colin <colinrozzi@gmail.com>
2025-05-23 15:51:01 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+mod bindings;
+
+use crate::bindings::exports::ntwk::theater::actor::Guest;
+use crate::bindings::ntwk::theater::runtime::{log, shutdown};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct State {
+    messages: Vec<String>,
+}
+
+struct Actor;
+impl Guest for Actor {
+    fn init(
+        _init_state_bytes: Option<Vec<u8>>,
+        params: (String,),
+    ) -> Result<(Option<Vec<u8>>,), String> {
+        log("Initializing hello-world actor");
+        let (self_id,) = params;
+        log(&format!("Actor ID: {}", &self_id));
+        log("Hello from hello-world actor!");
+
+        shutdown(None);
+
+        Ok((None,))
+    }
+}
+
+bindings::export!(Actor with_types_in bindings);
